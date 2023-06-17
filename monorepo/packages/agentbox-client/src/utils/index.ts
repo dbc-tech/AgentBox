e5b557322694da97b592fa2b8695cd796fd841d6
Neil Dobson <neil.dobson71@gmail.com>
2023-06-17 19:31:51 +1000

    Clean-up unused

--- monorepo/packages/agentbox-client/src/utils/index.ts
+++ monorepo/packages/agentbox-client/src/utils/index.ts
@@ -2,5 +2,3 @@ export type ObjectToUnion<T> = T[keyof T]
 export type Nillable<T> = T | undefined | null
 export type Nullable<T> = T | null
 export type Unpack<T> = T extends Array<infer U> ? U : T
-
-export * from './plain-to-dto'
